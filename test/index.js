06b16e3189c09dbf1d8b5820d360be8d055263a7
Stefan Stoichev <stefan.stoichev@gmail.com>
2026-06-05 11:20:24 +0100

    peggy version bump; drop block updated

--- test/index.js
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from "fs";
-import { parse } from "../dist/qlik-script-parser.js";
-
-const scriptFile = "./script_files/script1.qvs";
-const script = readFileSync(scriptFile).toString();
-
-const parsedText = parse(script);
-let a = 1;
