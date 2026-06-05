a94291c876652f0e7e59c36edad6524c5618952c
Stefan Stoichev <stefan.stoichev@gmail.com>
2026-06-05 11:20:39 +0100

    and the filess

--- src/build.js
+++ src/build.js
@@ -3,61 +3,61 @@ import peggy from "peggy";
 
 // List all grammar files. They will be read in the input order
 const blocksOrder = [
-  "main",
-  "unknown",
-  "when",
-  "hidden",
-  "connect",
-  "direct",
-  "qualify",
-  "section-star",
-  "sub-call",
-  "search",
-  "tag",
-  "tab",
-  "comment",
-  "let-set",
-  "if",
-  "load",
-  "load-prefixes",
-  "load-fields",
-  "load-source",
-  "load-suffixes",
-  "trace",
-  "drop",
-  "rename",
-  "do",
-  "loopUntil",
-  "exit",
-  "declare",
-  "derive",
-  "for",
-  "next",
-  "else",
-  "end",
-  "store",
-  "sleep",
-  "binary",
-  "expressions",
-  "functions",
-  "misc",
-  "base",
+    "main",
+    "unknown",
+    "when",
+    "hidden",
+    "connect",
+    "direct",
+    "qualify",
+    "section-star",
+    "sub-call",
+    "search",
+    "tag",
+    "tab",
+    "comment",
+    "let-set",
+    "if",
+    "load",
+    "load-prefixes",
+    "load-fields",
+    "load-source",
+    "load-suffixes",
+    "trace",
+    "drop",
+    "rename",
+    "do",
+    "loopUntil",
+    "exit",
+    "declare",
+    "derive",
+    "for",
+    "next",
+    "else",
+    "end",
+    "store",
+    "sleep",
+    "binary",
+    "expressions",
+    "functions",
+    "misc",
+    "base",
 ];
 
 const fullGrammar = blocksOrder
-  .map((b) => {
-    return readFileSync(`./blocks/${b}.peggy`);
-  })
-  .join("\n\r");
+    .map((b) => {
+        return readFileSync(`./blocks/${b}.peggy`);
+    })
+    .join("\n\r");
 
 // Once all files are read - write the complete grammar
 writeFileSync("../dist/qlik-grammar.peggy", fullGrammar);
 
 // Parse the complete grammar
 const parser = peggy.generate(fullGrammar, {
-  //   trace: true,
-  output: "source",
-  format: "es",
+    //   trace: true,
+    output: "source",
+    format: "es",
 });
 
 // Write the parser
